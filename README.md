89ce3cbe68821f5eb63548ec088ab5eae64bc706
John Sim <jsim@fishbowlsolutions.com>
2022-09-16 12:02:50 +0100

    Create Base

---
+++ README.md
@@ -0,0 +1,3 @@
+# OCM-Discussions-Widget (In Development...)
+
+Free Custom Component built with Svelte for creating threaded discussions against articles and blog posts with users in OCM can be used headless or installed as a component and used directly in OCM Sites.
---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<svelte:options tag="ocm-discussions-widget" />
+
+<script>
+	//svelte
+	import { onMount } from 'svelte';
+	
+
+	onMount(() => {
+	});
+
+</script>
+
+
+<!-- Wrapper -->
+<section class="bitmapbytes-threadedDiscussions">
+	
+</section>
+<!-- xWrapper -->
+
+<style>
+	
+	.bitmapbytes-threadedDiscussions ::-webkit-scrollbar {
+		display:block;
+		width:8px;
+	}
+	
+	.bitmapbytes-threadedDiscussions ::-webkit-scrollbar-thumb {
+		background: #7C8B9A; 
+		border-radius: 100px;
+	}
+
+	.bitmapbytes-threadedDiscussions ::-webkit-scrollbar-button {
+		width:8px;
+		height:10px;
+	}
+
+
+</style>
